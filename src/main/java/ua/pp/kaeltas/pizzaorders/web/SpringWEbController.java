b45453c53adfdbd7eab9cce5a02ba1b4af26c7a1
Andrii Matsokha <matsokha.andrey@gmail.com>
2015-08-09 23:24:45 +0300

    Add basic SpringMVC support

---
+++ src/main/java/ua/pp/kaeltas/pizzaorders/web/SpringWEbController.java
@@ -0,0 +1,16 @@
+package ua.pp.kaeltas.pizzaorders.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SpringWEbController {
+
+	@RequestMapping("hello")
+	@ResponseBody
+	public String printHello() {
+		return "Hello from Spring!";
+	}
+	
+}
