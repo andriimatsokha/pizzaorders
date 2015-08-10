769963294108361b5727ff20518d78f52593ad13
Andrii Matsokha <matsokha.andrey@gmail.com>
2015-08-10 12:06:01 +0300

    Add InternalResourceViewResolver support Add appContext.xml loading and context initializing at startup Add sample view of all pizzas retrieved from DB in JSP page

--- src/main/java/ua/pp/kaeltas/pizzaorders/web/SpringWEbController.java
+++ src/main/java/ua/pp/kaeltas/pizzaorders/web/SpringWEbController.java
@@ -1,16 +1,41 @@
 package ua.pp.kaeltas.pizzaorders.web;
 
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import ua.pp.kaeltas.pizzaorders.repository.PizzaRepository;
 
 @Controller
 public class SpringWEbController {
 
+	@Autowired
+	PizzaRepository pizzaRepository;
+	
 	@RequestMapping("hello")
 	@ResponseBody
 	public String printHello() {
 		return "Hello from Spring!";
 	}
+	
+	@RequestMapping("hello2")
+	public String printHello2() {
+		
+		return "helloSpring2";
+	}
+	
+	@RequestMapping("hello3")
+	public String printHello3(Model model) {
+		model.addAttribute("msg", "Hello Spring View Resolver with params");
+		model.addAttribute("date", new Date());
+		model.addAttribute("pizzas", pizzaRepository.getAllPizzas());
+		
+		return "helloSpring3";
+	}
+	
 	
 }
