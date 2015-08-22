d887f6fe9df1332cb80edc09c4001c50ad2b9409
Andrii Matsokha <matsokha.andrey@gmail.com>
2015-08-22 16:15:43 +0300

    Add 50% discount for highest price pizza, if count of pizzas more than 4

--- src/main/java/ua/pp/kaeltas/pizzaorders/domain/TotalOrderPriceCalculator.java
+++ src/main/java/ua/pp/kaeltas/pizzaorders/domain/TotalOrderPriceCalculator.java
@@ -5,9 +5,14 @@ import java.util.Map.Entry;
 
 public class TotalOrderPriceCalculator {
 
+	private static final float DISCHARGE_PERCENT_FOR_HIGHEST_PRICE_PIZZA = 50f/100f;
+	private static final int PIZZA_COUNT_TO_GET_DISCHARGE = 4;
+	private static final int MIN_COUNT_OF_PIZZAS_IN_ORDER = 1;
+	private static final int MAX_COUNT_OF_PIZZAS_IN_ORDER = 10;
+
 	/**
 	 * Constraints: order contains 1-10 pizzas, else - exception
-	 * If number of pizzas in order more than 4, then give discharge to the highest price pizza 
+	 * If number of pizzas in order more than 4, then give discharge 50% to the highest price pizza 
 	 * 
 	 * @param pizzas
 	 * @return sum of order
@@ -20,12 +25,24 @@ public class TotalOrderPriceCalculator {
 		for(Entry<Pizza, Integer> entry : pizzas.entrySet()) {
 			Pizza pizza = entry.getKey();
 			Integer pizzaCount = entry.getValue();
-			orderPrice += entry.getKey().getPrice() * entry.getValue();
+			orderPrice += pizza.getPrice() * pizzaCount;
 		}
 		
-		return orderPrice;
+		return orderPrice - calculatePizzaDischarge(pizzas);
 	}
 	
+
+	int calculatePizzaDischarge(Map<Pizza, Integer> pizzas) {
+		int maxPizzaPrice = 0;
+		if (calcTotalPizzasCount(pizzas) > PIZZA_COUNT_TO_GET_DISCHARGE) {
+			for(Entry<Pizza, Integer> entry : pizzas.entrySet()) {
+				Pizza pizza = entry.getKey();
+				maxPizzaPrice = Math.max(maxPizzaPrice, pizza.getPrice());
+			}
+		}
+		
+		return (int)(maxPizzaPrice*DISCHARGE_PERCENT_FOR_HIGHEST_PRICE_PIZZA);
+	}
 	
 	private int calcTotalPizzasCount(Map<Pizza, Integer> pizzas) {
 		int totalCount = 0;
@@ -38,8 +55,8 @@ public class TotalOrderPriceCalculator {
 	
 	private void validatePizzas(Map<Pizza, Integer> pizzas) {
 		if (pizzas.isEmpty() 
-				|| calcTotalPizzasCount(pizzas) > 10 
-				|| calcTotalPizzasCount(pizzas) < 1) {
+				|| calcTotalPizzasCount(pizzas) > MAX_COUNT_OF_PIZZAS_IN_ORDER 
+				|| calcTotalPizzasCount(pizzas) < MIN_COUNT_OF_PIZZAS_IN_ORDER) {
 			throw new IllegalArgumentException("No pizzas in order");
 		}
 		
--- src/test/java/ua/pp/kaeltas/pizzaorders/domain/TotalOrderPriceCalculatorTest.java
+++ src/test/java/ua/pp/kaeltas/pizzaorders/domain/TotalOrderPriceCalculatorTest.java
@@ -52,7 +52,7 @@ public class TotalOrderPriceCalculatorTest {
 		int pizzaPrice = 10;
 		pizzas.put(new Pizza(1, "4Cheese", pizzaPrice, PizzaType.VEGETARIAN), 5);
 		pizzas.put(new Pizza(2, "2Cheese", pizzaPrice, PizzaType.VEGETARIAN), 5);
-		int expectedOrderPrice = 100;
+		int expectedOrderPrice = 95; //100 - 5(discharge for max price pizza)
 		
 		TotalOrderPriceCalculator totalOrderCostCalculator = new TotalOrderPriceCalculator();
 		int actualOrderPrice = totalOrderCostCalculator.calculateTotalOrderPrice(pizzas);
@@ -77,6 +77,67 @@ public class TotalOrderPriceCalculatorTest {
 		pizzas.put(new Pizza(2, "2Cheese", pizzaPrice, PizzaType.VEGETARIAN), -2);
 		
 		new TotalOrderPriceCalculator().calculateTotalOrderPrice(pizzas);
+	}
+	
+	@Test
+	public void testCalculatePizzaDischargeWith5Pizzas() {
+		Map<Pizza, Integer> pizzas = new HashMap<>();
+		int pizzaPrice1 = 11;
+		int pizzaPrice2 = 22;
+		int pizzaPrice3 = 33;
+		int pizzaPrice4 = 50;
+		int pizzaPrice5 = 10;
+		pizzas.put(new Pizza(1, "4Cheese", pizzaPrice1, PizzaType.VEGETARIAN), 1);
+		pizzas.put(new Pizza(2, "3Cheese", pizzaPrice2, PizzaType.VEGETARIAN), 1);
+		pizzas.put(new Pizza(3, "2Cheese", pizzaPrice3, PizzaType.VEGETARIAN), 1);
+		pizzas.put(new Pizza(4, "1Cheese", pizzaPrice4, PizzaType.VEGETARIAN), 1);
+		pizzas.put(new Pizza(5, "0Cheese", pizzaPrice5, PizzaType.VEGETARIAN), 1);
+		int expectedPizzaDischarge = 25;
+		
+		TotalOrderPriceCalculator totalOrderCostCalculator = new TotalOrderPriceCalculator();
+		int actualPizzaDischarge = totalOrderCostCalculator.calculatePizzaDischarge(pizzas);
+		
+		assertEquals(expectedPizzaDischarge, actualPizzaDischarge);
+	}
+	
+	@Test
+	public void testCalculatePizzaDischargeWithTwoHighestPricePizzas() {
+		Map<Pizza, Integer> pizzas = new HashMap<>();
+		int pizzaPrice1 = 11;
+		int pizzaPrice2 = 22;
+		int pizzaPrice3 = 33;
+		int pizzaPrice4 = 50;
+		int pizzaPrice5 = 50;
+		pizzas.put(new Pizza(1, "4Cheese", pizzaPrice1, PizzaType.VEGETARIAN), 1);
+		pizzas.put(new Pizza(2, "3Cheese", pizzaPrice2, PizzaType.VEGETARIAN), 1);
+		pizzas.put(new Pizza(3, "2Cheese", pizzaPrice3, PizzaType.VEGETARIAN), 1);
+		pizzas.put(new Pizza(4, "1Cheese", pizzaPrice4, PizzaType.VEGETARIAN), 1);
+		pizzas.put(new Pizza(5, "0Cheese", pizzaPrice5, PizzaType.VEGETARIAN), 1);
+		int expectedPizzaDischarge = 25;
+		
+		TotalOrderPriceCalculator totalOrderCostCalculator = new TotalOrderPriceCalculator();
+		int actualPizzaDischarge = totalOrderCostCalculator.calculatePizzaDischarge(pizzas);
+		
+		assertEquals(expectedPizzaDischarge, actualPizzaDischarge);
+	}
+	
+	@Test
+	public void testCalculatePizzaDischargeWith4Pizzas() {
+		Map<Pizza, Integer> pizzas = new HashMap<>();
+		int pizzaPrice1 = 11;
+		int pizzaPrice2 = 22;
+		int pizzaPrice3 = 33;
+		int pizzaPrice4 = 43;
+		pizzas.put(new Pizza(1, "4Cheese", pizzaPrice1, PizzaType.VEGETARIAN), 1);
+		pizzas.put(new Pizza(2, "3Cheese", pizzaPrice2, PizzaType.VEGETARIAN), 1);
+		pizzas.put(new Pizza(3, "2Cheese", pizzaPrice3, PizzaType.VEGETARIAN), 1);
+		pizzas.put(new Pizza(4, "1Cheese", pizzaPrice4, PizzaType.VEGETARIAN), 1);
+		int expectedPizzaDischarge = 0;
+		
+		TotalOrderPriceCalculator totalOrderCostCalculator = new TotalOrderPriceCalculator();
+		int actualPizzaDischarge = totalOrderCostCalculator.calculatePizzaDischarge(pizzas);
+		
+		assertEquals(expectedPizzaDischarge, actualPizzaDischarge);
 	}
 	
 	
